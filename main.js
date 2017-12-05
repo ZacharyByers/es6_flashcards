c8dab94f06bb6697187f8e67d5cc884fc4ddf259
Zach Byers <byerszach@gmail.com>
2017-12-05 16:59:41 -0700

    add and remove functionality

--- main.js
+++ main.js
@@ -13,10 +13,18 @@ $(document).ready( function() {
       `<div class='col s3 card blue'>
         <p>${$hint.val()}</p>
         <h5 class='hidden'>${$answer.val()}</h5>
+        <div class='btn delete'>Delete</div>
       </div>
       <div class='col s1'></div>`
     )
     $hint.val('')
     $answer.val('')
+    Materialize.toast('Card Added', 3000)
+  })
+
+  $(document).on('click', '.delete', function() {
+    $(this).parent().next().remove()
+    $(this).parent().remove()
+    Materialize.toast('Card Removed', 3000)
   })
 })
